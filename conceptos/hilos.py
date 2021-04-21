89f9ba15956712839e26e613031e5d7a32742060
Ubuntu <ubuntu@ip-172-31-7-110.eu-west-1.compute.internal>
2021-04-21 11:14:38 +0000

    pings

--- conceptos/hilos.py
+++ conceptos/hilos.py
@@ -13,17 +13,28 @@ class Contador(Thread):
         self.nombre=nombre
         self.cantidad=cantidad
         self.delay=delay
+        self.debo_contar=False
     
     def cuenta(self):
+        self.debo_contar=True
         self.start()  
         
     def run(self): 
-        for actual in range(1,self.cantidad+1):
+#        for actual in range(1,self.cantidad+1):
+#            if self.debo_contar==False:
+#                return
+#            print ("Soy %s, voy por el %d" % (self.nombre,actual))
+#            time.sleep(self.delay) # El hilo que está ejecutando este trabajo
+
+        actual=0
+        while actual<= self.cantidad and self.debo_contar:
+            actual+=1
             print ("Soy %s, voy por el %d" % (self.nombre,actual))
             time.sleep(self.delay) # El hilo que está ejecutando este trabajo
 
+
     def deja_de_contar(self):
-        pass
+        self.debo_contar=False
 
     
 mi_contador_1=Contador("Gerardo",30,1)
@@ -36,5 +47,11 @@ mi_contador_2.cuenta()
 mi_contador_3.cuenta()
 
 # Una vez empiecen a contar, a los 5 segundos, paro el 1
+time.sleep(5)
+mi_contador_1.deja_de_contar()
 # A los 15 segundos, el siguiente,
+time.sleep(10)
+mi_contador_2.deja_de_contar()
 # A los 20 segundos el ultimo
+time.sleep(5)
+mi_contador_3.deja_de_contar()
---
+++ ejercicios/ping.py
@@ -0,0 +1,28 @@
+import platform    # For getting the operating system name
+import subprocess  # For executing a shell command
+
+def ping(host):
+    """
+    Returns True if host (str) responds to a ping request.
+    Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
+    """
+
+    # Option for the number of packets as a function of
+#    param ='-c'
+#    if platform.system().lower()=='windows' :
+#        param = '-n'
+    # Option for the number of packets as a function of
+#    if platform.system().lower()=='windows' :
+#        param = '-n'
+#    else:
+#        param ='-c'
+    # Option for the number of packets as a function of
+    parametro = '-n' if platform.system().lower()=='windows' else '-c'
+
+    # Building the command. Ex: "ping -c 1 google.com"
+    comando = ['ping', parametro, '1', host]
+    codigo_salida=subprocess.call(comando)
+        # Hace una llamada SINCRONA al comando
+        # Es decitr, se llama al comando, se espera a que termine.
+        # Al terminar, devuelve el código de salida del comando.
+    return codigo_salida==0
